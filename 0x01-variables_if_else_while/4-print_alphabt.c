96a6da8006c711dbb4517e0a76c80d42df313846
David Maigwa <davidmaigwa164@gmail.com>
2023-09-11 14:09:05 -0700

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: 'print alphabets'
+ * Return: success(0)
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
+
