055a2251f40fff65622e85f0cff0deb3e568c2d1
David Maigwa <davidmaigwa164@gmail.com>
2023-10-11 19:54:14 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,4 @@
-include "3-calc.h"
+#include "3-calc.h"
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -57,4 +57,3 @@ int op_mod(int a, int b)
 {
 return (a % b);
 }
-                                               1,1           Top
